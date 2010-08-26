c6ed2874585164183753bcb32fc63334d5d6958e
tav <tav@espians.com>
2010-08-26 23:27:11 +0100

    Fixed conflict in redis-cli::usage().

--- src/redis-cli.c
+++ src/redis-cli.c
@@ -401,13 +401,8 @@ static sds readArgFromStdin(void) {
 }
 
 static void usage() {
-<<<<<<< HEAD
     fprintf(stderr, "usage: redis-cli [-iv] [-h host] [-p port] [-s unix_domain_socket] [-a authpw] [-r repeat_times] [-n db_num] cmd arg1 arg2 arg3 ... argN\n");
     fprintf(stderr, "usage: echo \"argN\" | redis-cli -c [-h host] [-p port] [-s unix_domain_socket] [-a authpw] [-r repeat_times] [-n db_num] cmd arg1 arg2 ... arg(N-1)\n");
-=======
-    fprintf(stderr, "usage: redis-cli [-iv] [-h host] [-p port] [-a authpw] [-r repeat_times] [-n db_num] cmd arg1 arg2 arg3 ... argN\n");
-    fprintf(stderr, "usage: echo \"argN\" | redis-cli [-h host] [-p port] [-a authpw] [-r repeat_times] [-n db_num] cmd arg1 arg2 ... arg(N-1)\n");
->>>>>>> upstream/master
     fprintf(stderr, "\nIf a pipe from standard input is detected this data is used as last argument.\n\n");
     fprintf(stderr, "example: cat /etc/passwd | redis-cli set my_passwd\n");
     fprintf(stderr, "example: redis-cli get my_passwd\n");
